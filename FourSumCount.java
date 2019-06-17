ed5aee78d5e97f5f0d8f4091e86051d5a002e077
YANHongHu <49400532+YANHongHu@users.noreply.github.com>
2019-06-17 13:56:51 +0800

    四数相加（哈希表）
    借用哈希表先存储A、B中数的和以及他们各自出现的次数，再判断计算C、D中数的和，并判断哈希表中是否存在它的相反数......

--- FourSumCount.java
+++ FourSumCount.java
@@ -1,5 +1,7 @@
 package algorithm;
 
+import java.util.HashMap;
+
 public class FourSumCount {
 
 	public static void main(String[] args) {
@@ -10,19 +12,22 @@ public class FourSumCount {
 		System.out.println(fourSumCount(A, B, C, D));
 	}
 	public static int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
-        int num = A.length;
-        int count = 0;
-        for(int i=0;i<num;i++) {
-        	for(int j=0;j<num;j++) {
-        		for(int k=0;k<num;k++) {
-        			for(int l=0;l<num;l++) {
-        				if(A[i]+B[j]+C[k]+D[l]==0) {
-        					count++;
-        				}
-        			}
-        		}
-        	}
-        }
-		return count;
-    }
+		int count = 0;
+	    HashMap<Integer,Integer> hashMap = new HashMap<>();
+	    for (int numA : A) {
+	        for (int numB : B) {
+	            int sumAB = numA + numB;
+	            //计数A和B中不同值的和各自出现了多少次
+	            hashMap.put(sumAB, hashMap.getOrDefault(sumAB, 0) + 1);
+	        }
+	    }
+	    for (int numC : C){
+	        for (int numD : D){
+	            int sumCD = numC + numD;
+	            //判断在哈希表中是否存在与C、D中的数的和为相反数的AB和，如果则把它出现的次数加到count，没有则加0
+	            count += hashMap.getOrDefault(-sumCD,0);
+	        }
+	    }
+	    return count;
+	}
 }
